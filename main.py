ca5f132670125eb92c6a56d578ae658ccea4d81c
leo-fernandeslopes <leo.fernandeslopes@edu.esiee.fr>
2024-11-14 10:03:06 +0000

    un message explicatif

--- main.py
+++ main.py
@@ -1,16 +1,27 @@
+""" Dans main.py, on code la fonction ispalindrome(p) et ses tests automatiques. """
 #### Fonction secondaire
 
-
 def ispalindrome(p):
+    """
+    Elle prend en paramètre une String p et renvoie un booléen qui
+    affirme si p est un palindrome.
+    """
 
-    # votre code ici
-    
-    return False
+    p = p.lower()
+
+    accents = "àâäéèêëîïôöùûüÿç-',;.?:!_"
+    sans_accents = "aaaeeeeiioouuuyc         "
+
+    dico = str.maketrans(accents, sans_accents)
+    p = p.translate(dico).replace(" ", "")
+    return p == p[::-1]
 
 #### Fonction principale
 
 
 def main():
+    """
+    La procédure ne prend aucun paramètre et effectue les tests de palindrome(p). """
 
     # vos appels à la fonction secondaire ici
 
